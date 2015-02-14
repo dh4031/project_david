e1985893a0c238f3f658f35746646a8c6d0a8eaf
David Han <dh4031@yahoo.com>
2015-02-14 08:01:11 +0000

    added psuedocode

---
+++ psuedocode.py
@@ -0,0 +1,24 @@
+from __future__ import division 
+
+resistance = open("AR-genes.fa").read().rstrip(">")
+#calculation of the number of ATGC
+def antibiotic(resistance):
+	gene_length = len(resistance)
+	a_count = resistance_A.upper().count('A')
+	t_count = resistance_T.upper().count('T')
+	c_count = resistance_G.upper().count('G')
+	g_count = resistance_C.upper().count('C')
+	return round (a_count, t_count, c_count, g_count)
+#complementing the strand
+def complement_RNA(resistance):
+	a_complement = resistance
+
+#converting the strand of every 3 RNA sequence into an amino acid
+def amino_acid(RNA):
+
+#matching specific proteins
+def RNA(protein):
+
+#plan to list all the bacteria
+
+#retrieve information on all the types of antibiotics
